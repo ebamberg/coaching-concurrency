fd2772c6dbbff718541a77c496b18840cd5084aa
ebamberg <erik.bamberg@web.de>
2020-01-07 14:56:40 +0000

    Update CompletableFutureDemo.java

--- CompletableFutureDemo.java
+++ CompletableFutureDemo.java
@@ -9,4 +9,24 @@ public class CompletableFutureDemo {
         Future future=CompletableFuture.runAsync( ()->System.out.println("hello world"), ForkJoinPool.commonPool() );
     }
 
+public void exceptional() {
+    
+    CompletableFuture.supplyAsync( () -> {System.out.println("Hello World"); return 1; })
+        .exceptional( throwable-> {
+                                    throwable.printStackTrace(); 
+                                    return 0; 
+                                   }  );
+    
+    
+      Thread.sleep(3000);//let the stages complete
+      System.out.println("-- checking exceptions --");
+      boolean b = completableFuture.isCompletedExceptionally();
+      System.out.println("completedExceptionally: " + b);
+      System.out.println("-- getting results --");
+      int result = completableFuture.get();
+      System.out.println(result);
+    
+    
+}
+
 }
